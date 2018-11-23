ca58bc3739fe931dab6c0d57ce9feab77e9d139b
meothang <meothang@yahoo.com.vn>
2018-11-23 08:10:14 +0700

    Add a full_title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  	def hello
-  		render html: "hello world"
-  	end
+  protect_from_forgery with: :exception
+  before_action :set_locale
+  private
+  def default_url_options
+    {locale: I18n.locale}
   end
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,9 @@ Bundler.require(*Rails.groups)
 module DtProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
