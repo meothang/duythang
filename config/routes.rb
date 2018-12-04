e96c55958c5be0e7ee757a00ae954613d8d713aa
meothang <meothang@yahoo.com.vn>
2018-12-04 15:06:10 +0700

    Chuong7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get "users/new"
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get "/contact",to:"static_pages#contact"
     get "/help",to: "static_pages#help"
     get "/about",to: "static_pages#about"
     get "/signup",to: "users#new"
+    post "/signup",  to: "users#create"
+    resources :users
   end
 end
